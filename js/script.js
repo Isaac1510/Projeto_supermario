ba193adcb72cbe0147ab434d51e053cbcd712f20
Isaac Aires <isaacaires103@gmail.com>
2024-09-11 17:46:50 -0300

    1 versão

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const mario = document.querySelector('.mario');
+        const pipe = document.querySelector('.pipe');
+        const music = document.getElementById('marioMusic'); 
+        const gameOverMusic = document.getElementById('gameOverMusic');
+        
+        let score = 0;
+
+        
+        const playMusic = () => {
+            music.loop = true;  
+            music.play();     
+        }
+
+        
+        const jump = () => {
+            mario.classList.add('jump');
+
+            setTimeout(() => {
+                mario.classList.remove('jump');
+            }, 500);
+        }
+
+        
+        const loop = setInterval(() => {
+
+            console.log('loop')
+
+            const pipePosition = pipe.offsetLeft;
+            const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+
+            if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105)  {
+
+                pipe.style.animation = 'none';
+                pipe.style.left = `${pipePosition}px`;
+
+                mario.style.animation = 'none';
+                mario.style.bottom = `${marioPosition}px`;
+
+                mario.src = 'img/game-over.png';
+                mario.style.width = '75px'
+                mario.style.marginLeft = '50px'
+
+                clearInterval(loop);
+                
+                gameOverMusic.play();
+
+                music.pause(); 
+
+                return;
+            }
+
+            updateScore();
+
+        }, 1000);
+
+        
+        document.addEventListener('keydown', jump);
+        document.addEventListener('keydown', playMusic);  
+        
