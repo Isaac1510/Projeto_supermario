c3fa922ef9778b2489d8211a255f51711d89a986
Isaac Aires <isaacaires103@gmail.com>
2024-09-12 09:25:08 -0300

    Update script.js
    melhorando o codigo

--- js/script.js
+++ js/script.js
@@ -1,58 +1,65 @@
 const mario = document.querySelector('.mario');
-        const pipe = document.querySelector('.pipe');
-        const music = document.getElementById('marioMusic'); 
-        const gameOverMusic = document.getElementById('gameOverMusic');
-        
-        let score = 0;
+const pipe = document.querySelector('.pipe');
+const music = document.getElementById('marioMusic');
+const gameOverMusic = document.getElementById('gameOverMusic');
+const scoreElement = document.getElementById('score');
 
-        
-        const playMusic = () => {
-            music.loop = true;  
-            music.play();     
-        }
-
-        
-        const jump = () => {
-            mario.classList.add('jump');
-
-            setTimeout(() => {
-                mario.classList.remove('jump');
-            }, 500);
-        }
-
-        
-        const loop = setInterval(() => {
-
-            console.log('loop')
-
-            const pipePosition = pipe.offsetLeft;
-            const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
-
-            if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105)  {
-
-                pipe.style.animation = 'none';
-                pipe.style.left = `${pipePosition}px`;
-
-                mario.style.animation = 'none';
-                mario.style.bottom = `${marioPosition}px`;
-
-                mario.src = 'img/game-over.png';
-                mario.style.width = '75px'
-                mario.style.marginLeft = '50px'
-
-                clearInterval(loop);
-                
-                gameOverMusic.play();
-
-                music.pause(); 
-
-            }
+let score = 0;
+let gameRunning = true;
 
 
-        }, 10);
+const playMusic = () => {
+    music.loop = true;
+    music.play();
+}
 
-        
-        document.addEventListener('keydown', jump);
-        document.addEventListener('touchstart', jump);
-        document.addEventListener('keydown', playMusic);  
+
+const jump = () => {
+    if (gameRunning) {
+        mario.classList.add('jump');
+
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
+}
+
+
+const loop = setInterval(() => {
+
+    console.log('loop')
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'img/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+
+        gameOverMusic.play();
+        music.pause();
+
+    }
+
+    if (gameRunning) {
+        score += 1; // Incrementa a pontuação
+        scoreElement.textContent = `Score: ${score}`; // Atualiza o elemento com a pontuação
+    }
+
+}, 100);
+
+
+document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
+document.addEventListener('keydown', playMusic);  
         
